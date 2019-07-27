2cb753b0f865de12d1332b89848da6da17b772a9
appkr <juwonkim@me.com>
2019-07-28 01:25:35 +0900

    Composite pattern

--- src/Task.php
+++ src/Task.php
@@ -2,36 +2,12 @@
 
 namespace App;
 
-class Task
+class Task extends Todo
 {
-    private $title;
-    private $isDone;
-
-    public function __construct($title)
-    {
-        $this->title = $title;
-        $this->isDone = false;
-    }
-
     public function markDone()
     {
         $this->isDone = true;
         return $this;
-    }
-
-    public function getTitle()
-    {
-        return $this->title;
-    }
-
-    public function isDone()
-    {
-        return $this->isDone;
-    }
-
-    public function hashCode()
-    {
-        return spl_object_hash($this);
     }
 
     public function __toString()
@@ -44,5 +20,10 @@ Task {
     isDone: {$done}
 }
 EOT;
+    }
+
+    public function toHtml()
+    {
+        return "<li><a href='{$this->hashCode()}'>{$this->title}</a></li>";
     }
 }
