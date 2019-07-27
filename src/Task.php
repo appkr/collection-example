8fc0a3111bf959832237f1f79065cd0876297245
appkr <juwonkim@me.com>
2019-07-27 22:42:49 +0900

    Collection basics

---
+++ src/Task.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+class Task
+{
+    private $title;
+    private $isDone;
+
+    public function __construct($title)
+    {
+        $this->title = $title;
+        $this->isDone = false;
+    }
+
+    public function markDone()
+    {
+        $this->isDone = true;
+        return $this;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function isDone()
+    {
+        return $this->isDone;
+    }
+
+    public function __toString()
+    {
+        $isDone = $this->isDone ? "true" : "false";
+
+        $string = static::class . " {\n";
+        $string .= "    title: {$this->title}\n";
+        $string .= "    isDone: {$isDone}\n";
+        $string .= "}";
+
+        return $string;
+    }
+}
